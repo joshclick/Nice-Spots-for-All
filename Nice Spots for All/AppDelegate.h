d38aa59834ea388ebb73d90ce0728e9f70533cad
Josh Click <joshuaqian8@gmail.com>
2014-11-11 14:28:14 -0500

    Initial Commit

---
+++ Nice Spots for All/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nice Spots for All
+//
+//  Created by Josh Click on 11/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nice Spots for All/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nice Spots for All
+//
+//  Created by Josh Click on 11/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
