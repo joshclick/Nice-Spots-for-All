931fd45d18424a78401edc95d2f3029217346ee8
Abhijai <abhijai.garg@gmail.com>
2014-11-15 17:19:50 -0500

    adding a boilerplate for facebook login

---
+++ Facebook Login Sample/Facebook Login Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Facebook Login Sample
+//
+//  Created by Abhijai on 11/15/14.
+//  Copyright (c) 2014 abhijaigarg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Facebook Login Sample/Facebook Login SampleTests/Facebook_Login_SampleTests.m
@@ -0,0 +1,40 @@
+//
+//  Facebook_Login_SampleTests.m
+//  Facebook Login SampleTests
+//
+//  Created by Abhijai on 11/15/14.
+//  Copyright (c) 2014 abhijaigarg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Facebook_Login_SampleTests : XCTestCase
+
+@end
+
+@implementation Facebook_Login_SampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
