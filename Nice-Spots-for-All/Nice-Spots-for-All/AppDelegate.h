3890885611f91895923f89c9c6c65a1244bcec82
Abhijai <abhijai.garg@gmail.com>
2014-12-05 00:07:08 -0500

    removed facebook login scripts

---
+++ Nice-Spots-for-All/Nice-Spots-for-All/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nice-Spots-for-All
+//
+//  Created by Abhijai on 12/4/14.
+//  Copyright (c) 2014 abhijaigarg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nice-Spots-for-All/Nice-Spots-for-All/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nice-Spots-for-All
+//
+//  Created by Abhijai on 12/4/14.
+//  Copyright (c) 2014 abhijaigarg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
